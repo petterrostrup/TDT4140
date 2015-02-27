19077bf6c64ef8c0230925f4888c7a5e611bbac0
krskog <k.skog@online.no>
2015-02-27 11:30:50 +0100

    registrer YO MAMA

--- src/application/RegistrerController.java
+++ src/application/RegistrerController.java
@@ -4,9 +4,11 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
@@ -41,13 +43,41 @@ public class RegistrerController {
 	@FXML
 	private ImageView imageview;
 	
-	@Override
+	@FXML
+	private Label ugyldigBrukernavn;
+	
+	@FXML
+	private Label ugyldigEpost;
+	
 	public void start(Stage stage) throws Exception {
 		Parent root = FXMLLoader.load(getClass().getResource("registrer.fxml"));
+		Scene scene = new Scene(root);
 		stage.setTitle("Registrer");
 		stage.setScene(scene);
 		stage.show();
 		System.out.println("HELLO WØØØRLD");
 	}
 	
+	public void regButt (ActionEvent event){
+		System.out.println("registrert");
+		try{
+			new RegistrerController().start(new Stage());
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+		Node source = (Node) event.getSource();
+		Stage stage = (Stage) source.getScene().getWindow();
+		stage.close();
+	}
+	
+	public void openButt(ActionEvent event){
+		System.out.println("butt iz opened");
+	}
+	
+	
+	public static void main(String[] args){
+		launch(args);
+	}
+
 }
