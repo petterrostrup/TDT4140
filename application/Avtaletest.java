3297075873142edfc16a02e920b03a3e867d39d2
krskog <k.skog@online.no>
2015-03-02 13:10:22 +0100

    lagavtale setText

--- application/Avtaletest.java
+++ application/Avtaletest.java
@@ -2,4 +2,36 @@ package application;
 
 public class Avtaletest {
 
+
+//Adding a Label
+final Label label = new Label();
+GridPane.setConstraints(label, 0, 3);
+GridPane.setColumnSpan(label, 2);
+grid.getChildren().add(label);
+
+//Setting an action for the Submit button
+submit.setOnAction(new EventHandler<ActionEvent>() {
+
+@Override
+  public void handle(ActionEvent e) {
+      if ((comment.getText() != null && !comment.getText().isEmpty())) {
+          label.setText(name.getText() + " " + lastName.getText() + ", "
+              + "thank you for your comment!");
+      } else {
+          label.setText("You have not left a comment.");
+      }
+   }
+});
+
+//Setting an action for the Clear button
+clear.setOnAction(new EventHandler<ActionEvent>() {
+
+@Override
+  public void handle(ActionEvent e) {
+      name.clear();
+      lastName.clear();
+      comment.clear();
+      label.setText(null);
+  }
+});
 }
--- application/LagAvtaleController.java
+++ application/LagAvtaleController.java
@@ -2,12 +2,14 @@ package application;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.SplitMenuButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -42,6 +44,9 @@ public class LagAvtaleController extends Application {
 	
 	@FXML
 	private GridPane gridpane;
+	
+	@FXML
+	private Label label1;
 
 	@Override
 	public void start(Stage stage) throws Exception {
@@ -53,20 +58,32 @@ public class LagAvtaleController extends Application {
 	        stage.setScene(scene);
 	        stage.show();
 	        
+	        
+	        
+	        
 	}
 	
 	
 	public void lagreButt (ActionEvent event) {
 		System.out.println("test");
 		//Lagre data fra skjema i database
+		
+		Button lagreButt = new Button("Lagre");
+		lagreButt.setOnAction(new EventHandler<ActionEvent>(){
+			@Override
+			public void handle(ActionEvent e){
+				if ((tittel.getText() != null && !tittel.getText().isEmpty())) {
+					label1.setText(tittel.getText()); //utvides og kobles til korrekt LABEL/BUTTON(kalenderelement)
+				}
+				else{
+					label1.setText("Fyll in det feltet"); //kobles med feilmeldingslabels
+				}
+			}
+			
+		});
+	
 	
 	}
-	
-	
-	
-	
-	
-	
 	
 
 	public static void main(String[] args) {
