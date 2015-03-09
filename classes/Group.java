aa600a20478048dc492067c6d76ea4181613f125
petterrostrup <petterrostrup@gmail.com>
2015-03-09 14:46:09 +0100

    Member sql in place (almost)

--- classes/Group.java
+++ classes/Group.java
@@ -1,21 +1,50 @@
 package classes;
 
+import java.sql.ResultSet;
 import java.util.ArrayList;
 
 public class Group {
-	private String GroupID;
+	private String groupID;
+	private String groupName;
 	private ArrayList<User> persons;
 	
 	public String getGroupID() {
-		return GroupID;
+		return groupID;
 	}
 	public void setGroupID(String groupID) {
-		GroupID = groupID;
+		this.groupID = groupID;
+	}
+	public String getGroupName() {
+		return groupName;
+	}
+	public void setGroupName(String groupName) {
+		this.groupName = groupName;
 	}
 	public ArrayList<User> getPersons() {
 		return persons;
 	}
 	public void setPersons(ArrayList<User> persons) {
 		this.persons = persons;
+	}
+	
+	public void createGroup(){
+		String sqlStatement = "INSERT IGNORE INTO MEMBERGROUP (name) "
+				+ "VALUES (" + getGroupName() + ")";
+		ResultSet results = DatabaseCommunicator.execute(sqlStatement);
+	}
+	
+	public void addMember(int id){
+		String sqlStatement = "INSERT IGNORE INTO MEMBER (person, membergroup) "
+				+ "VALUES (" + id + ", " + this.getGroupID() + ")";
+		ResultSet results = DatabaseCommunicator.execute(sqlStatement);
+	}
+	
+	public void removeMember(int id){
+		String sqlStatement = "DELETE IGNORE FROM MEMBER WHERE person = " + id;
+		ResultSet results = DatabaseCommunicator.execute(sqlStatement);
+	}
+	
+	public void getMembers(){
+		
 	}
 }
