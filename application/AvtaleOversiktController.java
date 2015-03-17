4cb4c4348452a7affd2b11fbec7fe06eaa927db6
emilsa <emil-andersen@live.no>
2015-03-17 13:39:55 +0100

    stuff

--- application/AvtaleOversiktController.java
+++ application/AvtaleOversiktController.java
@@ -1,6 +1,10 @@
 package application;
+import java.sql.Timestamp;
+import java.util.Calendar;
+
 import classes.Appointment;
 import classes.Login;
+import classes.Room;
 import classes.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -34,39 +38,69 @@ public class AvtaleOversiktController {
 	
 	@FXML
 	private Button redigerAvtale;
+	
 
-	private Appointment nyApp;
+	//private Appointment nyApp;
+	
+	
+	
 	
 	@FXML
 	private void initialize(){
+		//Lager
+		Calendar c1 = Calendar.getInstance();
+		Room testRoom = new Room("5", "Realfag 245", "somewhere", 10);
+		
+		
+		Appointment appz = new Appointment("Gruppemøte", "Vanlig møte", "Bygg-1", testRoom,  c1.getTime() ,Timestamp.valueOf("2015-03-23 18:00:00.0"),Timestamp.valueOf("2015-03-23 20:00:00.0"), sessionUser);
+		//Appointment appz = new Appointment(null, null, null, testRoom, null, null, null, null, sessionUser);
+		
+
+		tittel.setText(appz.getName());
+		//rom.setText(appz.getRoom().toString()); må få vist romnavnet fra Room klassen
+		//deltagere.setText(appz.getParticipants().toString()); må vise en liste over deltagere
+		dato.setText(appz.getDate().toString());
+		tidspunkt.setText(appz.getStart().toString() + "-");
+		tidspunkt.setText(appz.getEnd() + "-" );
+		System.out.println(appz.getDescription());
+		beskrivelse.setText(appz.getDescription());
+		
 	}
 	
 //	private Appointment VisAvtale(){
 //		
-//		Appointment nyApp = null;
+//		Appointment visApp = null;
 //		
 //		//newuser = Login.login(brukernavn.getText(), passord.getText());
-//		nyApp = new Appointment(null, null, null, null, null, null, null, null, null);
+//		visApp = new Appointment(null, null, null, null, null, null, null, null, null);
 //		//nyApp.getName();
-//		System.out.println(nyApp.getName());
+//		System.out.println(visApp.getName());
 //		System.out.println("asd");
-//		tittel.setText(nyApp.getName());
+//		//tittel.setText(visApp.getName());
+//		tittel.setText("hei");
 //		
 //		
-//		return nyApp;
+//		return visApp;
 //		
 //	}
 	
 	
 	public Appointment redigerAvtale (ActionEvent event) {	
 		
+		//Calendar avt = Calendar.getInstance();
+		//avt.set
+		
+		
+		
 		Appointment nyApp = new Appointment(null, null, null, null, null, null, null, null, sessionUser);
+		//Appointment nyApp = new Appointment(null, null, null, null, null, null, null, sessionUser);
+		//Appointment nyApp = new Appointment();
 		
 		
 		try {
 			nyApp.getName();			
 			System.out.println(nyApp.getName());
-			System.out.println("asd");
+			System.out.println("tst");
 			
 						
 			
@@ -75,10 +109,30 @@ public class AvtaleOversiktController {
 			System.out.println("asd");
 			System.out.println(e);
 		
+		}if(nyApp != null){
+			System.out.println("Viser avtaleoversikt");
+			
+//			tittel.setText(null);
+//			rom.setText(null);
+//			deltagere.setText(null);
+//			dato.setText(null);
+//			tidspunkt.setText(null);
+//			beskrivelse.setText(null);
+			
+			
+//			Node  source = (Node)  event.getSource(); 
+//		    Stage stage  = (Stage) source.getScene().getWindow();
+//		    stage.close();
 		}
+		else{
+		
+		
+		
+		
 		return nyApp;
 		
-		
+		}
+		return nyApp;
 	}
 
 
