1b07a6424f34314167c36901be2bb9dc40d81d59
krskog <k.skog@online.no>
2015-03-20 20:58:21 +0100

    themes

--- application/ProfilController.java
+++ application/ProfilController.java
@@ -145,6 +145,7 @@ public class ProfilController {
 //			Application.setUserAgentStylesheet(null);
 //			// add custom global stylesheet
 //			StyleManager.getInstance().addUserAgentStylesheet("../style/DarkTheme.css");
+			
 			   
 		}
 //		else if(visThemes.getSelectionModel().getSelectedItem().equals("Girly Theme")){
